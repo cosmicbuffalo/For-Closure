69a677a8847308296e6a6fd82ffa314b53aa31dc
cosmicbuffalo <cosmic.buffalo.dev@gmail.com>
2017-05-27 14:30:54 -0400

    added some temporary controllers and stuff for front end, started building html and css for front landing page

---
+++ app/controllers/front_end_testing_controller.rb
@@ -0,0 +1,5 @@
+class FrontEndTestingController < ApplicationController
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'front_end_testing/index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
